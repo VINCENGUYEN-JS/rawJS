a6c54050646fee8e23f7cdbb04610d583fb35419
VinceNguyen <nguyenquangv95@gmail.com>
2022-05-31 20:59:37 +0700

    add-auto-scroll-with-request-animation-frame

--- smooth-scroll/index.js
+++ smooth-scroll/index.js
@@ -6,28 +6,43 @@ navbarLink.forEach((link) => {
 });
 
 //window.scrollTo
-function smoothScroll(event) {
-  event.preventDefault();
-  const targetId = event.currentTarget.getAttribute("href");
-  const targetElement = document.querySelector(targetId);
-  window.scrollTo({
-    top: targetId === "#" ? 0 : targetElement.offsetTop,
-    behavior: "smooth",
-  });
-}
+// function smoothScroll(event) {
+//   event.preventDefault();
+//   const targetId = event.currentTarget.getAttribute("href");
+//   const targetElement = document.querySelector(targetId);
+//   window.scrollTo({
+//     top: targetId === "#" ? 0 : targetElement.offsetTop,
+//     behavior: "smooth",
+//   });
+// }
 
 //window.scrollIntoView
+// function smoothScroll(event) {
+//   event.preventDefault();
+//   const targetId = event.currentTarget.getAttribute("href");
+//   const targetElement = document.querySelector(targetId);
+//   targetElement.scrollIntoView({
+//     behavior: "smooth",
+//     block: "start",
+//   });
+// }
+
+//window.requestAnimationFrame
 function smoothScroll(event) {
   event.preventDefault();
   const targetId = event.currentTarget.getAttribute("href");
-  const targetElement = document.querySelector(targetId);
-  targetElement.scrollIntoView({
-    behavior: "smooth",
-    block: "start",
-  });
-}
+  const offsetTop = document.querySelector(targetId).offsetTop;
+  const pageYOffset = window.pageYOffset;
+  const distance = offsetTop - pageYOffset;
+  const duration = 1000;
+  let start = null;
 
-//window.requestAnimationFrame
-function smoothScroll(event){
-  
+  window.requestAnimationFrame(step);
+
+  function step(timeStamp) {
+    if (!start) start = timeStamp;
+    const progress = timeStamp - start;
+    window.scrollTo(0, distance * (progress / duration) + pageYOffset);
+    if (progress < duration) window.requestAnimationFrame(step);
+  }
 }
