eeead717ced4d19d15890b7d39a684dcbbc19e0b
VinceNguyen <nguyenquangv95@gmail.com>
2022-06-06 17:26:49 +0700

    add-toggle-for-accordion

---
+++ accordion/index.js
@@ -0,0 +1,9 @@
+const accordionItemHeaders = document.querySelectorAll(
+  ".accordion-item-header"
+);
+
+accordionItemHeaders.forEach((accordionItemHeader) => {
+  accordionItemHeader.addEventListener("click", (event) => {
+    accordionItemHeader.classList.toggle("active");
+  });
+});
