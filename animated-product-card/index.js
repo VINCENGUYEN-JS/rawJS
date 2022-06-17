f6edd15307efc7c67f0b58582613cf4ab8bf30b5
VinceNguyen <nguyenquangv95@gmail.com>
2022-06-17 19:09:45 +0700

    add-toggle-animation-for-animated-product

---
+++ animated-product-card/index.js
@@ -0,0 +1,14 @@
+let circle = document.querySelector(".color-option");
+
+circle.addEventListener("click", (e) => {
+  let target = e.target;
+  console.log({ target });
+  if (target.classList.contains("circle")) {
+    circle.querySelector(".active").classList.remove("active");
+    target.classList.add("active");
+    document.querySelector(".main-images .active").classList.remove("active");
+    document
+      .querySelector(`.main-images .${target.id}`)
+      .classList.add("active");
+  }
+});
